4150ab0f15db43629c4b5171626c6e11943be4e4
Justin Monge <justin.mnge@gmail.com>
2024-09-24 21:06:07 -0300

    added constants.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import pygame
+from os.path import join
+from constants import *
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self, groups):
+        super().__init__(groups)
+        self.image = pygame.load(join('images', 'player', 'o.png')).convert_alpha()
+        self.rect = self.image.get_frect(center = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2))
+        self.direction = pygame.Vector2()
 
 # pygame setup
 pygame.init()
-display = pygame.display.set_mode((1280, 720))
+display = pygame.display.set_mode(WINDOW_WIDTH, WINDOW_HEIGHT)
 clock = pygame.time.Clock()
 running = True
 
