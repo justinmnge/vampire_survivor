23cbf21fbdeccb6ce59a49c0b45724a263168ab5
Justin Monge <justin.mnge@gmail.com>
2024-10-01 21:15:29 -0300

    added sprites.py and collisions for player and objects

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from settings import *
 from player import Player
+from sprites import *
+from random import randint
 
 class Game:
     def __init__(self):
@@ -12,9 +14,14 @@ class Game:
         
         # groups
         self.all_sprites = pygame.sprite.Group()
+        self.collision_sprites = pygame.sprite.Group()
         
         # sprites
-        self.player = Player((400, 300), self.all_sprites)
+        self.player = Player((400, 300), self.all_sprites, self.collision_sprites)
+        for i in range(6):
+            x, y = randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)
+            w, h = randint(60, 100), randint(50, 100)
+            CollisionSprite((x, y), (w, h), (self.all_sprites, self.collision_sprites))
     
     def run(self):
         while self.running:
@@ -34,7 +41,7 @@ class Game:
             self.all_sprites.draw(self.display_surface)
             pygame.display.update()
             
-        pygame.QUIT()
+        pygame.quit()
 
 if __name__ == '__main__':      
     game = Game()
